be9fa4eb6bd70f30f0e57ae654e724e9be3ba98e
Hennadii_Shpak <borchuga@gmail.com>
2022-09-28 10:57:25 +0300

    minor fix

--- src/app/features/start-page/start-page/start-page.component.ts
+++ src/app/features/start-page/start-page/start-page.component.ts
@@ -1,20 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { fromEvent, Observable, tap } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-start-page',
   templateUrl: './start-page.component.html',
   styleUrls: ['./start-page.component.scss']
 })
-export class StartPageComponent implements OnInit {
-  scrollObserver$!: Observable<Event>;
-  scrollHeight: number = 0;
-
-  ngOnInit(): void {
-    this.scrollObserver$ = fromEvent(document, 'scroll').pipe(
-      tap(() => this.scrollHeight = window.scrollY)
-    )
-  }
+export class StartPageComponent {
+  readonly fixedButtonVisibleHeight: number = 300;
 
   onUpBtn(): void {
     const nav = document.querySelector('.nav')!;
